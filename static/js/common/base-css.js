cbfd00f93649e986226b875f1928a4d35a537a4b
liuyingjie <1193418905@qq.com>
2018-06-01 17:20:47 +0800

    项目页面创建

--- static/js/common/base-css.js
+++ static/js/common/base-css.js
@@ -1,3 +1,3 @@
-document.writeln("<link rel='shortcut icon' href='/favicon.ico' type='image/x-icon'>");
+document.writeln("<link rel='icon' href='../static/images/logo.ico' type='image/x-icon'>");
 document.writeln("<link rel='stylesheet' href='../static/css/font.css'/>");
 document.writeln("<link rel='stylesheet' href='../static/css/xadmin.css'/>");
