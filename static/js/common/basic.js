14fd16bad7cf0aa3325b5dd33563e0322425910b
liuyingjie <1193418905@qq.com>
2018-06-04 17:00:28 +0800

    菜单配置 404 500页面配置 错误捕获

--- static/js/common/basic.js
+++ static/js/common/basic.js
@@ -2,6 +2,6 @@ $(function () {
     if(SESSIONID == "" || null == SESSIONID || USER == "" || null == USER) {
         //layer.msg("登陆已过期",{icon:7});
         //alert("登陆已过期");
-        window.location.href = "login.html";
+        top.location.href = "../login.html";
     }
 })
